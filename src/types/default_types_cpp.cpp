b84d9de41a52ae178ad67706cd70e7a4143c8fa0
Clark Rinker <cgrinker@gmail.com>
2017-06-15 18:18:07 -0700

    Boxing bool

--- src/types/default_types_cpp.cpp
+++ src/types/default_types_cpp.cpp
@@ -11,3 +11,27 @@ CRAFT_TYPE_DEFINE(std::string)
 	_.use<PStringer>().singleton<FunctionalStringer>( [](::craft::instance<std::string> _this) { return *_this; } );
 }
 
+// stdlib
+CRAFT_TYPE_DEFINE(bool)
+{
+	_.use<PIdentifier>().singleton<SimpleIdentifier>("bool");
+	_.use<PParse>().singleton<FunctionalParse>([](std::string s)
+	{
+		if (s == "true" || s == "True")
+		{
+			return instance<bool>::make(true);
+		}
+		else if (s == "false" || s == "False")
+		{
+			return instance<bool>::make(false);
+		}
+		else
+		{
+			throw stdext::exception(fmt::format("{0} is not corecable to type Bool", s));
+		}
+	});
+	_.use<PStringer>().singleton<FunctionalStringer>([](::craft::instance<bool> _this) 
+	{ 
+		return (_this.get()) ? "true" : "false";
+	});
+}
--- src/types/default_types_cpp.h
+++ src/types/default_types_cpp.h
@@ -2,5 +2,5 @@
 #include "common.h"
 
 CRAFT_TYPE_DECLARE(CRAFT_TYPE_EXPORTED, std::string);
-
+CRAFT_TYPE_DECLARE(CRAFT_TYPE_EXPORTED, bool)
 
