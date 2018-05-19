e6292b98b8d6fe2653529fd3d51c743f6db35812
Mason Bially <mason.bially@gmail.com>
2018-05-19 13:40:47 -0700

    Annotating mpir types.

--- src/types/boot/default_types_mpir.h
+++ src/types/boot/default_types_mpir.h
@@ -2,6 +2,6 @@
 #include "types/common.h"
 #include "types/core.h"
 
-CRAFT_TYPE_DECLARE(CRAFT_TYPES_EXPORTED, mpz_class);
-CRAFT_TYPE_DECLARE(CRAFT_TYPES_EXPORTED, mpq_class);
-CRAFT_TYPE_DECLARE(CRAFT_TYPES_EXPORTED, mpf_class);
+CRAFT_TYPE_DECLARE(CRAFT_TYPES_EXPORTED, mpz_class); // Integer
+CRAFT_TYPE_DECLARE(CRAFT_TYPES_EXPORTED, mpq_class); // Rational
+CRAFT_TYPE_DECLARE(CRAFT_TYPES_EXPORTED, mpf_class); // Float
